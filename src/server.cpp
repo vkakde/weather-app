09de8c05dfeff32c9c64ce0d292e642d7c5b060d
vkakde <vishwajeet.kakde@gmail.com>
2018-12-27 18:18:24 -0500

    Added server functionality to fetch weather data for location as cmd arg

--- src/server.cpp
+++ src/server.cpp
@@ -1,15 +1,11 @@
-//
-// httpget.cpp
-//
-// This sample demonstrates the HTTPClientSession and the HTTPCredentials classes.
-//
-// Copyright (c) 2005-2012, Applied Informatics Software Engineering GmbH.
-// and Contributors.
-//
-// SPDX-License-Identifier:	BSL-1.0
-//
+///\author Vishwajeet Kakde
+///\cite https://github.com/pocoproject/poco/blob/develop/Net/samples/httpget/src/httpget.cpp
 
+// standard includes
+#include <iostream>
+#include <string>
 
+// includes from POCO library
 #include "Poco/Net/HTTPClientSession.h"
 #include "Poco/Net/HTTPRequest.h"
 #include "Poco/Net/HTTPResponse.h"
@@ -19,73 +15,66 @@
 #include "Poco/Path.h"
 #include "Poco/URI.h"
 #include "Poco/Exception.h"
-#include <iostream>
 
-
+///\remark Not a good idea to imply so many namespaces
+using Poco::Exception;
+using Poco::Path;
+using Poco::StreamCopier;
+using Poco::URI;
 using Poco::Net::HTTPClientSession;
+using Poco::Net::HTTPMessage;
 using Poco::Net::HTTPRequest;
 using Poco::Net::HTTPResponse;
-using Poco::Net::HTTPMessage;
-using Poco::StreamCopier;
-using Poco::Path;
-using Poco::URI;
-using Poco::Exception;
 
+class server{
+	std::string APIKEY = "f6973a433d45ee52c637f99755a73e40";
 
-bool doRequest(Poco::Net::HTTPClientSession& session, Poco::Net::HTTPRequest& request, Poco::Net::HTTPResponse& response)
-{
-	session.sendRequest(request);
-	std::istream& rs = session.receiveResponse(response);
-	std::cout << response.getStatus() << " " << response.getReason() << std::endl;
-	if (response.getStatus() != Poco::Net::HTTPResponse::HTTP_UNAUTHORIZED)
-	{
+	public:
+	server(){};
+	server(const server& copy){};
+	~server(){};
+
+	std::string getAPIKey(){
+		return APIKEY;
+	}
+
+	void doRequest(Poco::Net::HTTPClientSession &session, Poco::Net::HTTPRequest &request, Poco::Net::HTTPResponse &response){
+		session.sendRequest(request);
+		std::istream &rs = session.receiveResponse(response);
+		// std::cout << response.getStatus() << " " << response.getReason() << std::endl;
 		StreamCopier::copyStream(rs, std::cout);
-		return true;
 	}
-	else
-	{
-		Poco::NullOutputStream null;
-		StreamCopier::copyStream(rs, null);
-		return false;
-	}
-}
+};
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	if (argc != 2)
-	{
+	if (argc != 2){
 		Path p(argv[0]);
 		std::cout << "usage: " << p.getBaseName() << " <uri>" << std::endl;
 		std::cout << "       fetches the resource identified by <uri> and print it to the standard output" << std::endl;
 		return 1;
 	}
 
-	try
-	{
-		URI uri(argv[1]);
-		std::string path(uri.getPathAndQuery());
-		if (path.empty()) path = "/";
+	try{
+		// create server instance
+		server svr;
 
-		std::string username;
-		std::string password;
-		Poco::Net::HTTPCredentials::extractCredentials(uri, username, password);
-		Poco::Net::HTTPCredentials credentials(username, password);
+		// build uri
+		URI uri("http://api.openweathermap.org/data/2.5/weather");
+		uri.addQueryParameter("q", argv[1]);
+		uri.addQueryParameter("APPID", svr.getAPIKey());
+
+		std::string path(uri.getPathAndQuery());
+		if (path.empty())
+			path = "/";
 
 		HTTPClientSession session(uri.getHost(), uri.getPort());
 		HTTPRequest request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
 		HTTPResponse response;
-		if (!doRequest(session, request, response))
-		{
-			credentials.authenticate(request, response);
-			if (!doRequest(session, request, response))
-			{
-				std::cerr << "Invalid username or password" << std::endl;
-				return 1;
-			}
-		}
+
+		svr.doRequest(session, request, response);
 	}
-	catch (Exception& exc)
+	catch (Exception &exc)
 	{
 		std::cerr << exc.displayText() << std::endl;
 		return 1;
