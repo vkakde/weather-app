1d29d1d6f3fa7cf29cc855166b4a0c8747f71d28
vkakde <vishwajeet.kakde@gmail.com>
2018-12-30 17:28:28 -0500

    Added support for single client requesting weather data to server. Successfully tested. Partial JSON support, no caching (mongo) currently.

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,15 +1,39 @@
+///\author Vishwajeet Kakde
+///\cite https://github.com/booksbyus/zguide/blob/master/examples/C%2B%2B/hwclient.cpp
+
 #include<iostream>
 #include<string>
+#include "zmq.hpp"
 
 using namespace std;
 
-int main(){
+int main ()
+{
+    //  Prepare our context and socket
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REQ);
+
+    std::cout << "Connecting to server..." << std::endl;
+    socket.connect ("tcp://localhost:5555");
 
     string input = "";
 
     while(input!="q"){
-        cout<<"\nEnter city name (\'q\' to quit): ";
-        cin>>input;
+        cout<<"\n\nEnter city name (\'q\' to quit): ";
+        getline(cin, input);
+
+        // create request and send message 
+        zmq::message_t request (input.length());
+        memcpy (request.data (), input.c_str(), input.length());
+        socket.send (request);
+
+        // get server reply
+        zmq::message_t reply;
+        socket.recv (&reply);
+		///\cite Convert cstring to string: https://stackoverflow.com/a/8960101
+		std::string s_reply((char*)reply.data());
+
+        cout<<"Reply from server: " + s_reply;
     }
 
     return 0;
--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,8 +1,10 @@
 ///\author Vishwajeet Kakde
+///\cite https://github.com/booksbyus/zguide/blob/master/examples/C%2B%2B/hwserver.cpp
 
 // standard includes
 #include <iostream>
 #include <string>
+#include <unistd.h>
 
 // includes from POCO Net library
 #include "Poco/Net/HTTPClientSession.h"
@@ -20,6 +22,9 @@
 #include "Poco/JSON/ParseHandler.h"
 #include "Poco/JSON/JSONException.h"
 
+// includes from ZMQ
+#include "zmq.hpp"
+
 using Poco::Exception;
 using Poco::Path;
 using Poco::StreamCopier;
@@ -32,30 +37,79 @@ using namespace Poco::JSON;
 
 class server{
 	std::string APIKEY = "f6973a433d45ee52c637f99755a73e40";
+	zmq::context_t *context;
+	zmq::socket_t *socket;
 
 	public:
 	server(){};
 	server(const server& copy){};
-	~server(){};
+	~server(){
+		// delete zmq context and socket
+		delete(context);
+		delete(socket);
+	};
+
+	void init(){
+	    //  Prepare context and socket
+    	context = new zmq::context_t(1);
+    	socket = new zmq::socket_t(*context, ZMQ_REP);
+    	socket->bind("tcp://*:5555");
+	}
+
+	zmq::socket_t* getSocket(){
+		return socket;
+	}
+
+	zmq::context_t* getContext(){
+		return context;
+	}
 
 	std::string getAPIKey(){
 		return APIKEY;
 	}
 
+	std::string getCityWeatherData(std::string cityName, HTTPResponse &response){
+		// build uri
+		URI uri("http://api.openweathermap.org/data/2.5/weather");
+		uri.addQueryParameter("q", cityName);
+		uri.addQueryParameter("APPID", APIKEY);
+
+		std::string path(uri.getPathAndQuery());
+		if (path.empty())
+			path = "/";
+
+		// create http req/res objects
+		HTTPClientSession session(uri.getHost(), uri.getPort());
+		HTTPRequest request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+
+		// send HTTP request and record response head and response body
+		std::string s_responseBody = http_doHTTPRequest(session, request, response);
+
+		// Convert the response head to ostream, then to string
+		///\remark Response body not needed - could be retained/returned if needed
+		/*
+		std::stringbuf sb;
+		std::ostream os(&sb);
+		response.write(os);
+		std::string s_responseHead = sb.str();
+		*/
+
+		return s_responseBody;
+	}
+
 	///\cite https://github.com/pocoproject/poco/blob/develop/Net/samples/httpget/src/httpget.cpp
-	std::string doHTTPRequest(Poco::Net::HTTPClientSession &session, Poco::Net::HTTPRequest &request, Poco::Net::HTTPResponse &response){
+	std::string http_doHTTPRequest(Poco::Net::HTTPClientSession &session, Poco::Net::HTTPRequest &request, Poco::Net::HTTPResponse &response){
 		session.sendRequest(request);
 		std::istream &is = session.receiveResponse(response);
 		std::stringbuf sb;
 		std::ostream os(&sb);
 		StreamCopier::copyStream(is, os);
-		// std::cout << response.getStatus() << " " << response.getReason() << std::endl;
 		// StreamCopier::copyStream(is, std::cout);
 		return sb.str();
 	}
 
 	///\cite https://stackoverflow.com/a/18776035
-	std::string getValue(Object::Ptr aoJsonObject, std::string key) {
+	std::string json_getValue(Object::Ptr aoJsonObject, std::string key) {
     	Poco::Dynamic::Var loVariable;
     	std::string lsReturn;
   
@@ -69,44 +123,41 @@ class server{
 
 int main(int argc, char **argv)
 {
-	if (argc != 2){
-		Path p(argv[0]);
-		std::cout << "usage: " << p.getBaseName() << " <uri>" << std::endl;
-		std::cout << "       fetches the resource identified by <uri> and print it to the standard output" << std::endl;
-		return 1;
-	}
-
 	try{
 		// create server instance
 		server svr;
 
-		// build uri
-		URI uri("http://api.openweathermap.org/data/2.5/weather");
-		uri.addQueryParameter("q", argv[1]);
-		uri.addQueryParameter("APPID", svr.getAPIKey());
+		// initialize server
+		svr.init();
 
-		std::string path(uri.getPathAndQuery());
-		if (path.empty())
-			path = "/";
+		// begin accepting client requests
+    	while (true) {
+        	zmq::message_t request;
 
-		// create http req/res objects
-		HTTPClientSession session(uri.getHost(), uri.getPort());
-		HTTPRequest request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
-		HTTPResponse response;
+        	//  Wait for next request from client
+        	svr.getSocket()->recv(&request);
+			///\cite Convert cstring to string: https://stackoverflow.com/a/8960101
+			std::string s_request((char*)request.data());
 
-		// send HTTP request and record response head and response body
-		std::string s_responseBody = svr.doHTTPRequest(session, request, response);
+        	std::cout << "Received " + s_request + "\n";
 
-		// Convert the response head to ostream, then to string
-		std::stringbuf sb;
-		std::ostream os(&sb);
-		response.write(os);
-		std::string s_responseHead = sb.str();
-		
-		// TEST
-		std::cout<<"\nResponse head-- \n"<<s_responseHead;
-		std::cout<<"Response body-- \n"<<s_responseBody;
-	
+        	// Process request
+			HTTPResponse response;
+			std::string responseBody = svr.getCityWeatherData(s_request, response);
+
+			// Send response to client
+			if(response.getStatus()!=Poco::Net::HTTPResponse::HTTP_OK){
+				std::cout<<"\nFailed. HTTP Response Code: "<<response.getStatus();
+			}
+			else{
+	        	//  Send reply back to client
+    	    	zmq::message_t reply (responseBody.length());
+        		memcpy (reply.data (), responseBody.c_str(), responseBody.length());
+        		svr.getSocket()->send (reply);
+			}
+    	}
+
+		/*
 		// Parse the JSON and get the Results
 		Parser loParser;
     	Poco::Dynamic::Var loParsedJson = loParser.parse(s_responseBody);
@@ -117,13 +168,15 @@ int main(int argc, char **argv)
 
 		// Get values for specific keys of the JSON object (name = city, main = key weather metrics)
 	    std::cout<<"\n\nKey-value pairs of parsed JSON -- \n";
-		std::cout << "Name: " << svr.getValue(loJsonObject, "name");
-	    std::cout << "\nMain: " << svr.getValue(loJsonObject, "main");
+		std::cout << "Name: " << svr.json_getValue(loJsonObject, "name");
+	    std::cout << "\nMain: " << svr.json_getValue(loJsonObject, "main");
+		*/
 	}
 	catch (Exception &exc)
 	{
 		std::cerr << exc.displayText() << std::endl;
 		return 1;
 	}
+	
 	return 0;
 }
