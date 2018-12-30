20998c7cd0b70c825615d8c4611b4f4e74798da1
vkakde <vishwajeet.kakde@gmail.com>
2018-12-30 15:56:40 -0500

    Added support for ZMQ

--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
-#export library paths
-export LIBRARY_PATH=$PWD/ext/poco-1.9.0/lib/Linux/x86_64
-export LD_LIBRARY_PATH=$PWD/ext/poco-1.9.0/lib/Linux/x86_64
-
 #export environmental vars to external libs for use by cmake
 export POCO_HOME=$PWD/ext/poco-1.9.0
+export ZMQ_HOME=$PWD/ext/libzmq
+export CPPZMQ_HOME=$PWD/ext/cppzmq
+
+#export library paths
+# POCO libs
+export LIBRARY_PATH=$POCO_HOME/lib/Linux/x86_64
+export LD_LIBRARY_PATH=$POCO_HOME/lib/Linux/x86_64
+# ZMQ libs
+export LIBRARY_PATH=$LIBRARY_PATH:$ZMQ_HOME/builds/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ZMQ_HOME/builds/lib
